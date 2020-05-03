4a3d36f0590c7cf6265729cb53792879d4c02ecc
Orestis Zekai <zekai.orestis@hapimag.com>
2020-05-03 20:30:22 +0300

    Play song initialized

--- src/youtube.py
+++ src/youtube.py
@@ -1,5 +1,41 @@
+import vlc
+import pafy
 from general import init_youtube_client
+
+
+def make_query(_youtube, query):
+    _req = _youtube.search().list(q=query, part='snippet', type='video')
+    return _req.execute()
+
+
+def parse_response(resp):
+    try:
+        return resp['items'][0]['id']['videoId']
+    except KeyError:
+        return '0'
+
+
+def play_song(_id):
+    url = 'https://www.youtube.com/watch?v=' + str(_id)
+    video = pafy.new(url)
+    best = video.getbest()
+    play_url = best.url
+
+    instance = vlc.Instance()
+    player = instance.media_player_new()
+    media = instance.media_new(play_url)
+    media.get_mrl()
+    player.set_media(media)
+    player.play()
 
 
 if __name__ == '__main__':
     youtube = init_youtube_client()
+
+    q = input('Enter the song you want to play: ')
+
+    res = make_query(youtube, q)
+    video_id = parse_response(res)
+
+    if video_id != '0':
+        play_song(video_id)
